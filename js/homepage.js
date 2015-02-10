ac04572fe7f01ba81b0435badfbfb38ad083a300
Adam Noffsinger <adam.noffsinger@gmail.com>
2015-02-09 21:53:19 -0800

    Added particles.js, removed some styling elements, changed header

--- js/homepage.js
+++ js/homepage.js
@@ -1,19 +1,71 @@
 $(document).ready(function() {
 
+    $('.intro-line').animate({
+        "bottom":"15px",
+        "opacity":"1"
+    },400);
+
     $(".project-list-all").hide();
 
-// Logo Flip
-    var sOffset = $(".flip-container").offset().top;  //find top of logo
-    var shareheight = $(".flip-container").height(); //find height of logo
-    var $document = $(document);
-    var $flipper = $(".flip-container .flipper");
-
-    $(window).scroll(function() {
-        var scrollYpos = $document.scrollTop();
-        var direction = scrollYpos > 0? "180deg" : "0deg"; //questionmark is shorthand if
-
-        $flipper.css("-webkit-transform", "rotateY("+direction+")");
+    particlesJS('particles-js', {
+        particles: {
+            color: '#FFB9A6',
+            shape: 'circle',
+            opacity: 1,
+            size: 1,
+            size_random: false,
+            nb: 100,
+            line_linked: {
+                enable_auto: true,
+                distance: 500,
+                color: '#FFB9A6',
+                opacity: .5,
+                width: 1,
+                condensed_mode: {
+                    enable: false,
+                    rotateX: 600,
+                    rotateY: 600
+                }
+            },
+            anim: {
+                enable: true,
+                speed: 1
+            }
+        },
+        interactivity: {
+            enable: false,
+            mouse: {
+                distance: 250
+            },
+            detect_on: 'canvas',
+            mode: 'grab',
+            line_linked: {
+                opacity: 0.5
+            },
+            events: {
+                onclick: {
+                    push_particles: {
+                        enable: true,
+                        nb: 30
+                    }
+                }
+            }
+        },
+        retina_detect: true
     });
+
+// Logo Flip
+    // var sOffset = $(".flip-container").offset().top;  //find top of logo
+    // var shareheight = $(".flip-container").height(); //find height of logo
+    // var $document = $(document);
+    // var $flipper = $(".flip-container .flipper");
+    //
+    // $(window).scroll(function() {
+    //     var scrollYpos = $document.scrollTop();
+    //     var direction = scrollYpos > 0? "180deg" : "0deg"; //questionmark is shorthand if
+    //
+    //     $flipper.css("-webkit-transform", "rotateY("+direction+")");
+    // });
 
 // See More
     var $allProjects = $(".project-list-all");
