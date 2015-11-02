21204438ab36c2023505d635d3cc8897a0f44296
Adam Noffsinger <adam.noffsinger@gmail.com>
2015-11-01 20:53:51 -0800

    Finished refactoring SASS components folder, added blog empty state, changed all URLs

--- js/script.js
+++ js/script.js
@@ -2,32 +2,31 @@ $(document).ready(function() {
 
 	// Common Variables
 	menuSelector = $(".menu-selector");
-	overlay = $(".content-overlay");
 	content = $(".content")
 	headerItems = $(".header-items")
 	main = $(".main")
 	body = $("body")
 	menuItem = $(".menu-item")
-	headerTitle = $(".header-title > h2")
+	headerTitle = $(".header > h2")
 
 	loadDelay = 150
 
 
 	$('.loader').addClass("loader-finished");
 
-	headerTitle.addClass("header-title-loaded");
-	headerTitle.addClass("header-title-loaded");
+	headerTitle.addClass("loaded-x");
 
-	$('.intro > img').addClass("intro-loaded");
-	$('.intro > h1').addClass("intro-loaded");
-	$('.intro > p').addClass("intro-loaded");
-	$('.button-intro').addClass("intro-loaded");
+	$('.intro > img').addClass("loaded");
+	$('.intro > h1').addClass("loaded");
+	$('.intro > p').addClass("loaded");
+	$('.button-intro').addClass("loaded");
 
 
 	// $('.about > p').addClass("loaded");
 
-	$('.work-post-intro-desc').addClass("work-post-intro-loaded");
+	$('.work-post-intro-desc > p').addClass("loaded");
 	$('.blog-post-intro-desc > img').addClass("loaded");
+	$('.blog-post-preview-empty').addClass("loaded");
 
 	$('.header-main').addClass("header-main-loaded");
 	// receding footer
@@ -77,19 +76,20 @@ $(document).ready(function() {
 	    if(Math.abs(lastScrollTop - st) <= delta)
 	        return;
 
-	    // If they scrolled down and are past the navbar height, add class .nav-up.
+	    // If they scrolled down and are past the navbar height, add class .project-nav-up.
 	    if (st > lastScrollTop && st > navHeight){
 	        // Scroll Down
-	        $('.project-nav').addClass('nav-up');
+	        $('.project-nav').addClass('project-nav-up');
 			$('.intro-arrow').addClass('intro-arrow-hidden');
 	    } else if (st < 44) {
-			$('.project-nav').removeClass('nav-up');
+			$('.project-nav').removeClass('project-nav-up');
+			$('.intro-arrow').removeClass('intro-arrow-hidden');
 		}
 
 		if (posNav > posFooter) {
-	        $('.project-nav').removeClass('nav-up');
+	        $('.project-nav').removeClass('project-nav-up');
 	    } else if (posNav < posFooter && st > navHeight) {
-			$('.project-nav').addClass('nav-up');
+			$('.project-nav').addClass('project-nav-up');
 		}
 	    lastScrollTop = st;
 	}
@@ -99,7 +99,7 @@ $(document).ready(function() {
 	$("li.work-item").each(function(i, el) {
     var $this = $(this);
     setTimeout(function() {
-            $this.addClass('work-item-loaded');
+            $this.addClass('loaded');
         }, i*loadDelay);
 	});
 
@@ -114,18 +114,13 @@ $(document).ready(function() {
 	$(".blog-post-preview").each(function(i, el) {
 	var $this = $(this);
 	setTimeout(function() {
-			$this.addClass('work-item-loaded');
+			$this.addClass('loaded');
 		}, i*loadDelay);
 	});
 
-	$(".content-description").each(function(i, el) {
-	var $this = $(this);
-	setTimeout(function() {
-			$this.addClass('work-item-loaded');
-		}, i*loadDelay);
-	});
 
-	$(".nav-items-link").click(function (e) {
+
+	$(".menu-items-link").click(function (e) {
 
 		e.preventDefault();                   // prevent default anchor behavior
 		$(menuSelector).toggleClass("menu-selector-open");
@@ -138,75 +133,15 @@ $(document).ready(function() {
 	         window.location = goTo;
 	    }, 300);
 	});
-		 // store anchor href
-	// 	headerTitle.hide();
-		// menuSelector.toggleClass("open");
-		// nav.toggleClass("nav-open");
-		// overlay.toggleClass("open");
-		// canvas.toggleClass("open");
-		// content.toggleClass("open");
-		// headerItems.toggleClass("open");
-		// main.toggleClass("open");
-		// body.toggleClass("no-scroll");
-		// $(".intro").toggleClass("open");
-		// $(".post-content").toggleClass("open");
-		// menuSelector.toggleClass("menu-selector-open");
-		// $(".project-nav").toggleClass("open");
-		// $('.post-header').toggleClass('open');
 
-		// if (overlay.css('display') == "none") {
-		// 	overlay.css("display","block");
-		// 	overlay.fadeTo(300, .85);
-		//
-		// } else {
-		// 	overlay.fadeTo(300, 0);
-		// 	setTimeout(function() {
-		// 		overlay.css("display","none");
-		// 	},300);
-		// }
-		//
-		// $(".menu-top-bar").toggleClass("top-bar-close");
-		// $(".menu-middle-bar").toggleClass("middle-bar-close");
-		// $(".menu-bottom-bar").toggleClass("bottom-bar-close");
-
-
-
-
-
-	// overlay.click(function() {
-	// 	overlay.fadeTo(300, 0);
-	// 	setTimeout(function() {
-	// 		overlay.css("display","none");
-	// 	},300);
-	//
-	// 	// menuSelector.toggleClass("open");
-	// 	nav.toggleClass("nav-open");
-	// 	// overlay.toggleClass("open");
-	// 	canvas.toggleClass("open");
-	// 	// content.toggleClass("open");
-	// 	headerItems.toggleClass("open");
-	// 	main.toggleClass("open");
-	// 	body.toggleClass("no-scroll");
-	// 	$(".intro").toggleClass("open");
-	// 	$(".post-content").toggleClass("open");
-	// 	menuSelector.toggleClass("menu-selector-open");
-	// 	$(".project-nav").toggleClass("open");
-	//
-	// 	$(".menu-top-bar").toggleClass("top-bar-close");
-	// 	$(".menu-middle-bar").toggleClass("middle-bar-close");
-	// 	$(".menu-bottom-bar").toggleClass("bottom-bar-close");
-	//
-	// 	$('.menu-items').toggleClass('slide-in');
-	// 	$('.post-header').toggleClass('open');
-	// });
 
 	menuSelector.click(function() {
 
 		$("#menu").toggleClass("menu-visible");
-		$(".nav-items li:nth-of-type(1)").toggleClass("nav-item-loaded");
-		$(".nav-items li:nth-of-type(2)").toggleClass("nav-item-loaded");
-		$(".nav-items li:nth-of-type(3)").toggleClass("nav-item-loaded");
-		$(".nav-items li:nth-of-type(4)").toggleClass("nav-item-loaded");
+		$(".menu-items li:nth-of-type(1)").toggleClass("menu-item-loaded");
+		$(".menu-items li:nth-of-type(2)").toggleClass("menu-item-loaded");
+		$(".menu-items li:nth-of-type(3)").toggleClass("menu-item-loaded");
+		$(".menu-items li:nth-of-type(4)").toggleClass("menu-item-loaded");
 
 		$(menuSelector).toggleClass("menu-selector-open");
 		$(".menu-top-bar").toggleClass("top-bar-close");
@@ -216,98 +151,10 @@ $(document).ready(function() {
 		body.toggleClass("no-scroll");
 
 
-		// menuSelector.toggleClass("open");
-		// nav.toggleClass("nav-open");
-		// // overlay.toggleClass("open");
-		// canvas.toggleClass("open");
-		// // content.toggleClass("open");
-		// headerItems.toggleClass("open");
-		// main.toggleClass("open");
-		// body.toggleClass("no-scroll");
-		// $(".intro").toggleClass("open");
-		// $(".post-content").toggleClass("open");
-		// menuSelector.toggleClass("menu-selector-open");
-		// $(".project-nav").toggleClass("open");
-		// $('.post-header').toggleClass('open');
-		//
-		// if (overlay.css('display') == "none") {
-		// 	overlay.css("display","block");
-		// 	overlay.fadeTo(300, .85);
-		//
-		// } else {
-		// 	overlay.fadeTo(300, 0);
-		// 	setTimeout(function() {
-		// 		overlay.css("display","none");
-		// 	},300);
-		// }
 
-
-
-		// var url = window.location.href; // Get current url
-		// console.log(url);
-		// $('a[href="'+url+'"]').toggleClass('menu-active');
-
-		// Select an a element that has the matching href and apply a class of 'active'. Also prepend a - to the content of the link
-
-		// $('.menu-items').toggleClass('slide-in');
 	});
 
-	// Download project documentation button
-	// $(".project-nav-download-bubble").hide().delay("1000").fadeToggle("1000", function() {
-    //     $(this).delay("3000").fadeToggle("1000");
-    // });
 
-	// Hide project nav on scroll to footer
-    // $(window).scroll(function() {
-    //     var $nav = $(".project-nav");
-    //     var $textLogo = $(".text-logo");
-    //     var $footer = $("#contact");
-	//
-    //     var posFooter = $footer.offset().top;  //find top of footer
-    //     var posNav = $nav.offset().top + 350;  //find top of nav, and compensate for body of nav (260)
-	//
-    //     if (posNav > posFooter){
-    //         $(".project-nav, .text-logo").stop(true, true).animate({opacity: 0}, 100);
-    //     } else {
-    //         $(".project-nav, .text-logo").stop(true, true).animate({opacity: 1}, 100);
-    //     }
-    // });
 
-	// $.simpleWeather({
-	// 	location: 'San Francisco, CA',
-	// 	woeid: '',
-	// 	unit: 'f',
-	// 	success: function(weather) {
-	// 	html = '<p>'+weather.temp+'&deg;'+weather.units.temp+'</p>';
-	//
-	// 	$("#weather").html(html);
-	// 	},
-	// 	error: function(error) {
-	// 	$("#weather").html('<p>'+error+'</p>');
-	// 	}
-	// });
-
-	// Check all elements with a .target class against all images on a page
-	// BackgroundCheck.init({
-	// targets: '.bar'
-	// });
-
-	// Specific images
-	// BackgroundCheck.init({
-	// targets: '.bar',
-	// images: '.check-me'
-	// });
-
-	// All targets
-	// BackgroundCheck.refresh();
-
-	// Specific target
-	// BackgroundCheck.refresh(target);
-
-	// Get current targets
-	// BackgroundCheck.get('targets');
-	//
-	// // Change targets
-	// BackgroundCheck.set('targets', '.header');
-
+	footer.addClass('loaded-o');
 });
